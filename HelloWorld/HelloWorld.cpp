a6ef7090d8b5861737a1ad8e200cc45096a3112f
n_sato <n.satou@monochrome-corp.com>
2021-07-12 16:11:37 +0900

    ソースコードコンフリクトテスト

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -7,6 +7,8 @@ int main()
 	printf("Hello World\n");
 	printf("Hello World 2!!\n");
 	printf("Hello World 3!!\n");
+	printf("Hello World 4_Local!!\n");
+	printf("Hello World 5_Local!!\n");
 
 	// collaboratorとしてprivateアカウントからコミット
 	// Rebase Test Commit
